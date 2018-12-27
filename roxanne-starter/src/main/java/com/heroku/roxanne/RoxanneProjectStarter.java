30e87547b2d785b3da402128a7cba814e8f6c412
dawid <otherstuff@interia.pl>
2018-12-28 00:58:50 +0100

    port changes

--- roxanne-starter/src/main/java/com/heroku/roxanne/RoxanneProjectStarter.java
+++ roxanne-starter/src/main/java/com/heroku/roxanne/RoxanneProjectStarter.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.heroku.roxanne"})
+//@ComponentScan(basePackages = {"com.heroku.roxanne"})
 @EnableJpaRepositories(basePackages = {"com.heroku.roxanne"})
 //@Import({WebConfig.class})
 public class RoxanneProjectStarter {
